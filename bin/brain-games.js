87a3da29b3842d7dfcf69b6e80eb42c74efb0e97
ruslan.portnov <rooflame@yandex.ru>
2021-08-26 13:43:28 +0300

    update brain-games

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+import { whatName } from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+
+const currentName = whatName('May I have your name? ');
+console.log(`Hello, ${currentName}!`);
